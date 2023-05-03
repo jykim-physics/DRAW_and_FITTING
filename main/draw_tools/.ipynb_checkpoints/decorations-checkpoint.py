e1e6f169c30bbc4d80541662b881573dedc8de8c
jykim-physics <245d57634@gmail.com>
2023-05-04 01:38:03 +0900

    new fitting codes

---
+++ main/draw_tools/.ipynb_checkpoints/decorations-checkpoint.py
@@ -0,0 +1,48 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def b2helix(n):
+    return sns.cubehelix_palette(n, start=1.5, rot=1.5, dark=0.3, light=.8, reverse=True)
+def watermark(t=None,logo="Belle II", px=0.033, py=0.915, fontsize=16, alpha=0.8, alpha_logo=0.95, shift=0.15, bstyle='italic', *args, **kwargs):
+       
+    """
+
+    Args:
+        t:
+        logo:
+        px:
+        py:
+        fontsize:
+        alpha:
+        shift:
+        *args:
+        **kwargs:
+
+    Returns:
+
+    """
+    if t is None:
+        import datetime
+        t = " %d (group)" % datetime.date.today().year
+
+    plt.text(px, py, logo, ha='left',
+             transform=plt.gca().transAxes,
+             fontsize=fontsize,
+             style=bstyle,
+             alpha=alpha_logo,
+             weight='bold',
+             *args, **kwargs,
+             # fontproperties=font,
+             # bbox={'facecolor':'#377eb7', 'alpha':0.1, 'pad':10}
+             )
+    plt.text(px + shift, py, t, ha='left',
+             transform=plt.gca().transAxes,
+             fontsize=fontsize,
+             #          style='italic',
+             alpha=alpha,  *args, **kwargs
+             #          fontproperties=font,
+             # bbox={'facecolor':'#377eb7', 'alpha':0.1, 'pad':10}
+             )  
+    
+def lumi(l="$5\; \mathrm{pb}^{-1}$", px=0.033, py=0.839,  *args, **kwargs):
+    plt.text(px, py, "$\int\,L\,\mathrm{dt}\;=\;$" + l, transform=plt.gca().transAxes, *args, **kwargs )
