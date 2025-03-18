6c3fa01cb68990fbd8dca7b366c7fdce89a77b49
Jaeyoung Kim <245d57634@gmail.com>
2025-03-18 17:06:04 +0900

    20250318 new yhep

---
+++ main/FITTING/etapip/MC15rd_generic/opt_v7_fit_v1_bdt_Dp_CMS_p_new_range/generate_submit_file.sh
@@ -0,0 +1,26 @@
+echo "
+universe             = vanilla
+getenv               = true
+output               = log/job_summary_\$(Cluster)_\$(Process).out
+error                = log/job_summary_\$(Cluster)_\$(Process).error
+log                  = log/job_summary_\$(Cluster)_\$(Process).log
+request_cpus         = 2
+request_memory       = 4000
+executable           = python3.sh
+transfer_input_files = \$(job_script)
+arguments            = \$(job_script) \$(sign)
+" > job.sub
+
+for script in etapip_gg_offset_novo_CB.py etapip_pipipi_offset_novo_CB.py; do
+    for value in $(seq 0.61 0.01 0.90); do
+        echo "$script all $value" >> job_list.txt
+    done
+done
+
+for script in etapip_gg_K_offset_CB.py etapip_pipipi_K_offset_CB.py; do
+    for value in $(seq 0.70 0.01 0.99); do
+        echo "$script all $value" >> job_list.txt
+    done
+done
+
+echo "queue job_script, sign from job_list.txt" >> job.sub
