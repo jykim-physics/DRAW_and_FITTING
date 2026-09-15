e352af5ad42726f0a2e2083f078403defecf2ed4
Jaeyoung Kim <245d57634@gmail.com>
2026-09-15 17:15:09 +0900

    20260915 yhep

---
+++ main/MVA/MC16rd_EtaPrime/pipipi/condor/only_Dp/my_submission_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#source /cvmfs/belle.cern.ch/tools/b2setup release-08-01-02
+#source /cvmfs/belle.cern.ch/el7/tools/b2setup light-2409-toyger
+#source /cvmfs/belle.cern.ch/el7/tools/b2setup light-2411-aldebaran
+source /cvmfs/belle.cern.ch/tools/b2setup light-2607-kasei
+python3 train.py
+python3 draw.py
